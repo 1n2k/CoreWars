3cdc3e43fb6728165294a7dddce82a305705d074
Philip Wellnitz <philipwellnitz@gmx.de>
2013-12-04 15:45:12 +0100

    Changed some things for those ugly Win devs

--- CoreWars/Core.cs
+++ CoreWars/Core.cs
@@ -2,52 +2,55 @@ using System;
 
 namespace CoreWars
 {
-	/// <summary>
-	/// Core.
-	/// </summary>
-	public class Core
-	{
-		/// <summary>
-		/// The owner.
-		/// </summary>
-		public readonly Player owner;
+    namespace Engine
+    {
+        /// <summary>
+        /// Core.
+        /// </summary>
+        public class Core
+        {
+            /// <summary>
+            /// The owner.
+            /// </summary>
+            public readonly Player owner;
 
-		int position;
+            int position;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="CoreWars.Core"/> class.
-		/// </summary>
-		/// <param name='player'>
-		/// The owning player.
-		/// </param>
-		/// <param name='startposition'>
-		/// The startposition of the core.
-		/// </param>
-		public Core (Player player,int startposition)
-		{
-			this.owner = player;
-			this.position = startposition;
-		}
+            /// <summary>
+            /// Initializes a new instance of the <see cref="CoreWars.Core"/> class.
+            /// </summary>
+            /// <param name='player'>
+            /// The owning player.
+            /// </param>
+            /// <param name='startposition'>
+            /// The startposition of the core.
+            /// </param>
+            public Core(Player player, int startposition)
+            {
+                this.owner = player;
+                this.position = startposition;
+            }
 
-		/// <summary>
-		/// Runs the cell the core is pointing at.
-		/// </summary>
-		/// <returns>
-		/// True if the core survived.
-		/// </returns>
-		public bool runActualCell ()
-		{
-			Game game = Game.game;
-			switch (game[this.position].opcode) {
-				//opcodes here
-			case "JMP":
+            /// <summary>
+            /// Runs the cell the core is pointing at.
+            /// </summary>
+            /// <returns>
+            /// True if the core survived.
+            /// </returns>
+            public bool runActualCell()
+            {
+                Game game = Game.game;
+                switch (game[this.position].opcode)
+                {
+                    //opcodes here
+                    case "JMP":
 
-			case "NOP":
-			default:
-				break;
-			}
-			return true;
-		}
-	}
+                    case "NOP":
+                    default:
+                        break;
+                }
+                return true;
+            }
+        }
+    }
 }
-
