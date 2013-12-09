b4070fd6d5d54fa72dc2eed252e0724dbc5ad73f
Ernst Mueller <ernst-007@gmx.de>
2013-12-09 10:09:20 +0100

    Changed GUI-Form

--- CoreWars/GUI.Designer.cs
+++ CoreWars/GUI.Designer.cs
@@ -30,21 +30,34 @@
             /// </summary>
             private void InitializeComponent()
             {
+                this.textBox1 = new System.Windows.Forms.TextBox();
                 this.SuspendLayout();
+                // 
+                // textBox1
+                // 
+                this.textBox1.Location = new System.Drawing.Point(10, 109);
+                this.textBox1.Multiline = true;
+                this.textBox1.Name = "textBox1";
+                this.textBox1.Size = new System.Drawing.Size(68, 80);
+                this.textBox1.TabIndex = 0;
                 // 
                 // GUI
                 // 
                 this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
                 this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-                this.ClientSize = new System.Drawing.Size(292, 273);
+                this.ClientSize = new System.Drawing.Size(309, 274);
+                this.Controls.Add(this.textBox1);
                 this.Name = "GUI";
                 this.Text = "Form1";
                 this.Load += new System.EventHandler(this.GUI_Load);
                 this.ResumeLayout(false);
+                this.PerformLayout();
 
             }
 
             #endregion
+
+            private System.Windows.Forms.TextBox textBox1;
         }
     }
 }
